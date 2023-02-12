ad60655fb99e51f55185469695c1881e8f0daa3d
Andy Tran <aktran321@gmail.com>
2023-02-12 05:42:52 -0500

    added views, serializers, for menu and booking

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework",
     "restaurant",
     "bcrypt",
     "django.contrib.admin",
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,10 @@
 #define URL route for index() view
 from django.urls import path
 from . import views
+from .views import MenuView, BookingView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', MenuView.as_view()),
+    path('booking/', BookingView.as_view()),
 ]
